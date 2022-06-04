9d05bc1f107880d3f1044fb1f0ef3762a76726da
Gavin Peng <gpeng@oicr.on.ca>
2022-06-03 21:25:06 -0400

    calculate.sh path error

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -5,5 +5,5 @@ set -euo pipefail
 cd $1
 
 #find all fastq.gz files, return their md5sums to std out, list all file types
-find ./fastq -name *.fastq.gz -xtype f -exec sh -c "zcat {} | grep -v ^# | md5sum" \;
+find ./MMinden_9161/fastq -name *.fastq.gz -xtype f -exec sh -c "zcat {} | grep -v ^# | md5sum" \;
 ls ./fastq | sed 's/.*\.//' | sort | uniq -c
